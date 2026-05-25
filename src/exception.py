d4b83661d48c5e67eb7e8c379dafc774639584cb
yas1423 <yashxsingh17@gmail.com>
2026-05-25 14:10:44 +0530

    EDA and Problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,21 +1,28 @@
 import sys
-import logging
+from src.logger import logging
 
-def error_message_details(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in pyhton script [{0}] line number [{1}] and error message [{2}]".format(
-        file_name,exc_tb.tb_lineno,str(error)
+
+def error_message_details(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+
+    file_name = exc_tb.tb_frame.f_code.co_filename
+
+    error_message = (
+        f"Error occurred in python script [{file_name}] "
+        f"line number [{exc_tb.tb_lineno}] "
+        f"error message [{str(error)}]"
     )
+
     return error_message
 
+
 class CustomException(Exception):
-    def __init__(self, error_message,error_detail:sys):
+    def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
-        self.error_message=error_message_details(error_message,error_detail=error_detail)
+        self.error_message = error_message_details(error_message, error_detail)
 
-
-    def __str__(self) :
+    def __str__(self):
         return self.error_message
+
 
 
--- src/logger.py
+++ src/logger.py
@@ -3,20 +3,18 @@ import os
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+# only folder path
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
+
+# full file path
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
-
-filename=LOG_FILE_PATH,
-format= '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-level=logging.INFO
-
-
+    filename=LOG_FILE_PATH,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
 )
-
-
 
 
